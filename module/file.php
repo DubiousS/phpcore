483eff60adaeb59bdc4248419096fbddd33b21e5
Dima Mih <dj.dumah@yandex.ru>
2017-02-06 19:22:21 +0700

    060220171922

---
+++ module/file.php
@@ -0,0 +1,56 @@
+<?php 
+
+class FileControll
+{
+
+	public $image;
+
+	public function UploadImages($dir = "", $size = 2097152) {
+
+		$blacklist = array(".php", ".phtml", ".php3", ".php4", ".js", ".exe");
+		foreach ($blacklist as $item) {
+			if(preg_match("/$item\$/i", $this->image['name'])) {
+				echo "Ошибка при загрузки файла.\n";
+				exit;
+			}
+		}
+		if($this->image['size'] > $size) {
+			echo "Файл слишком большого размера.";
+			exit;
+		}
+
+		$temp = $this->getWidth() / $this->getHeight();
+		if($temp > 5 || $temp < 0.2) {
+			echo "Некорректная ширина или высота.";
+			exit;
+		}
+
+
+		$uploaddir = '../www/'.$dir;
+		$uploadfile = $uploaddir . basename($this->image['name']);
+
+
+		if (move_uploaded_file($this->image['tmp_name'], $uploadfile)) {
+			echo "Файл успешно загружен.\n";
+		} else {
+			echo "Ошибка при загрузки файла.\n";
+		}
+
+	}
+	public function getWidth() {
+		return getimagesize($this->image['tmp_name'])[0];
+	}
+	public function getHeight() {
+		return getimagesize($this->image['tmp_name'])[1];
+	}
+
+
+	public function ResizeImagesScale($value = "100") {
+		$width = $this->getWidth() * $value/100;
+		$height = $this->getHeight() * $value/100;
+
+		
+	}
+}
+
+?>
--- www/index.php
+++ www/index.php
@@ -1,5 +1,6 @@
 <?php
 require '../module/main.php';
+require '../module/file.php';
 $s = new main();
 $s->Page();
 ?>
