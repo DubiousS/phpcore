b575cda53ef5c2064e0d428ca3a231281621f657
Dima Mih <dj.dumah@yandex.ru>
2017-02-08 13:07:05 +0700

    0802171706

--- module/file.php
+++ module/file.php
@@ -4,24 +4,7 @@ class FileControll
 {
 	public $image;
 	public $type;
-	public $file;
 
-	public function UploadDoc() {
-		$whitelist = array(".doc", ".docx");
-		$i = 0;
-		foreach ($whitelist as $item) {
-			if(preg_match("/$item\$/i", $this->image['name']) == 1) {
-				$this->type = $item;
-				$i++;
-				break;
-			}
-		}
-		if($i == 0) {
-			echo "Ошибка при загрузки файла.\n";
-			exit;
-		}
-
-	}
 
 	public function UploadImages($size = 2097152) {
 
@@ -29,25 +12,37 @@ class FileControll
 		$i = 0;
 		foreach ($whitelist as $item) {
 			if(preg_match("/$item\$/i", $this->image['name']) == 1) {
+				if($item = '.jpg') $item = ".jpeg";
+				$temp = getimagesize($this->image['tmp_name'])['mime'];
+				$temp = stristr($temp, '/');
+				$temp = '.'.substr($temp, 1);
+				if($temp != $item) {
+					unset($this->image);
+					echo "Ошибка при загрузки файла.\n";
+					exit;
+				}
 				$this->type = $item;
 				$i++;
 				break;
 			}
 		}
 		if($i == 0) {
+			unset($this->image);
 			echo "Ошибка при загрузки файла.\n";
 			exit;
 		}
 		if($this->image['size'] > $size) {
+			unset($this->image);
 			echo "Файл слишком большого размера.";
 			exit;
 		}
 		$temp = getimagesize($this->image['tmp_name'])[0] / getimagesize($this->image['tmp_name'])[1];
 		if($temp > 5 || $temp < 0.2) {
+			unset($this->image);
 			echo "Некорректная ширина или высота.";
 			exit;
 		}
-		if($this->type == ".jpeg" || $this->type == ".jpg") $this->image = imagecreatefromjpeg($this->image['tmp_name']);
+		if($this->type == ".jpeg") $this->image = imagecreatefromjpeg($this->image['tmp_name']);
 		else if($this->type == ".png") $this->image = imagecreatefrompng($this->image['tmp_name']);
 		else if($this->type == ".gif") $this->image = imagecreatefromgif($this->image['tmp_name']);
 		
@@ -55,12 +50,14 @@ class FileControll
 		return 1; 
 	}
 	public function SaveImage($dir, $name) {
-		$uploaddir = '../www/'.$dir;
-		$uploadfile = $uploaddir .''. $name .''.$this->type ;
-		if($this->type = ".jpeg" || $this->type = ".jpg") $this->image = imagejpeg($this->image, $uploadfile);
-		else if($this->type = ".png") $this->image = imagepng($this->image, $uploadfile);
-		else if($this->type = ".gif") $this->image = imagegif($this->image, $uploadfile);
-		echo "Файл успешно загружен.";
+		if(isset($this->image)) {
+			$uploaddir = '../www/'.$dir;
+			$uploadfile = $uploaddir .''. $name .''.$this->type ;
+			if($this->type = ".jpeg") $this->image = imagejpeg($this->image, $uploadfile);
+			else if($this->type = ".png") $this->image = imagepng($this->image, $uploadfile);
+			else if($this->type = ".gif") $this->image = imagegif($this->image, $uploadfile);
+			echo "Файл успешно загружен.";
+		}
 	}
 	private function getWidth() {
 		return imagesx($this->image);
@@ -69,8 +66,7 @@ class FileControll
 		return imagesy($this->image);
 	}
 	public function ResizeImagesScale($value = "100") {
-		if($value != 100){
-
+		if($value != 100 && isset($this->image)) {
 			$width = $this->getWidth() * $value/100;
 			$height = $this->getHeight() * $value/100;
 			$new_image = imagecreatetruecolor($width, $height);
